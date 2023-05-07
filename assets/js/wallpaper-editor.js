b600d0a4f81ef6f2ba8a3c5ae12b8b41c3e4b853
Thomas Bryk <thomasbryk@cmail.carleton.ca>
2023-05-07 03:00:41 -0400

    Initial commit for multi-league (not working)

--- assets/js/wallpaper-editor.js
+++ assets/js/wallpaper-editor.js
@@ -1,5 +1,5 @@
-var nhlJson, selectedTeam, schedule;
-
+var selectedTeam, selectedLeague, schedule;
+var leagues = null;
 var scheduleRetrieved = false;
 var dropdownsPopulated = false;
 var teamSlider_mouseDownX = null;
@@ -11,31 +11,70 @@ $(document).ready(function() {
 });
 
 function onload() {
-    fetch("leagues/nhl/nhl.json")
+    fetch("leagues/leagues.json")
         .then(response => response.json())
-        .then(json => nhlJson = json)
-        .then(PostJsonRetrieval);
+        .then(json => leagues = json.leagues[0])
+        .then(() => {
+            let promises = [];
+
+            $.each(leagues, (i) => {
+                promises.push(RetrieveLeague(leagues[i]));
+            });
+
+            Promise.all(promises).then(Setup);
+        });
 }
 
-function PostJsonRetrieval() {
-    if (!nhlJson) return;
+async function RetrieveLeague(league) {
+    return new Promise((resolve) => {
+        fetch(league.path + league.jsonPath)
+            .then(response => response.json())
+            .then(json => league["teams"] = json.teams)
+            .then(resolve);
+    });
+}
 
+function Setup() {
     PopulateTeams();
     PopulateTimeZones();
 }
 
-
 function PopulateTeams() {
-    let $teamsDiv = $(".teamsCarousel");
+    let $leaguesDiv = $("#leaguesCarousel");
+    let $mainDiv = $("#main");
 
-    nhlJson.teams.sort((a, b) => a.name.localeCompare(b.name))
+    $.each(leagues, (i) => {
+        let currLeague = leagues[i];
 
-    jQuery.each(nhlJson.teams, (id, teamData) => {
-        let html = '<a name="' + teamData.id + '" class="link">\
-                        <article class="team" style=\'background-image: url("leagues/nhl/logos/' + teamData.id + '/Primary.png"); background-color: ' + Object.values(teamData.colours)[0] + ';\'></article>\
+        let leagueHtml = '<a name="' + currLeague.id + '" class="team link">\
+                        <article class="team";\>' + currLeague.name + '</article>\
                     </a>';
 
-        $teamsDiv.append(html);
+        $leaguesDiv.append(leagueHtml);
+
+        let $newTeamDiv = $('<div id="' + currLeague.id + '" class="teamsCarousel inner"></div>');
+        $mainDiv.append($newTeamDiv);
+
+        let teams = currLeague.teams;
+        teams.sort((a, b) => a.name.localeCompare(b.name))
+
+        $.each(teams, (id, teamData) => {
+            let teamHtml = '<a name="' + teamData.id + '" league=' + currLeague.id + ' class="team link">\
+                        <article class="team" style=\'background-image: url("' + currLeague.path + 'logos/' + teamData.id + '/Primary.png"); background-color: ' + Object.values(teamData.colours)[0] + ';\'></article>\
+                    </a>';
+
+            $newTeamDiv.append(teamHtml);
+        });
+
+    });
+
+    $('#leaguesCarousel').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: false,
+        autoplaySpeed: 4000,
+        swipe: true,
+        touchMove: true,
     });
 
     $('.teamsCarousel').slick({
@@ -63,9 +102,9 @@ function PopulateTeams() {
         ]
     });
 
-    let $teams = $('.link');
+    let $teams = $('.team');
 
-    jQuery.each($teams, (i, team) => {
+    $.each($teams, (i, team) => {
         $(team).mousedown((e) => {
             teamSlider_mouseDownX = e.pageX;
         });
@@ -73,7 +112,8 @@ function PopulateTeams() {
         $(team).mouseup((e) => {
             if (teamSlider_mouseDownX >= e.pageX - 3 && teamSlider_mouseDownX <= e.pageX + 3) {
                 let teamId = $(e.target).parent().attr('name');
-                TeamSelected(teamId);
+                let leagueId = $(e.target).parent().attr('league');
+                TeamSelected(teamId, leagueId);
             }
         });
     });
@@ -96,7 +136,7 @@ function PopulateLogos() {
     $logo.empty();
 
     let logos = selectedTeam.logos;
-    jQuery.each(logos, (key, logo) => {
+    $.each(logos, (key, logo) => {
         let option = new Option(key, logo);
 
         if (key == "Primary")
@@ -136,12 +176,14 @@ function PopulateStyles() {
     }
 }
 
-function TeamSelected(teamId) {
+function TeamSelected(teamId, leagueId) {
     if (selectedTeam && selectedTeam.id == teamId) return; //Same team, skip
+
+    selectedLeague = findLeagueById(leagueId);
 
     $("#wallpaper-viewer").addClass("spinner");
 
-    selectedTeam = nhlJson.teams.find(team => team.id == teamId);
+    selectedTeam = selectedLeague.teams.find(team => team.id == teamId);
 
     scheduleRetrieved = false;
     dropdownsPopulated = false;
@@ -151,12 +193,52 @@ function TeamSelected(teamId) {
 
     var wallpaperEditor = $("#wallpaper-selector");
     $('html,body').animate({ scrollTop: wallpaperEditor.offset().top }, 'slow').promise().done(() => {
-        RetrieveSchedule();
         PopulateLogos();
+        RetrieveSchedule();
     });
 }
 
 function RetrieveSchedule() {
+    switch (selectedLeague.id) {
+        case 1:
+            RetrieveSchedule_MLB();
+        case 2:
+            RetrieveSchedule_NHL();
+    }
+
+}
+
+function RetrieveSchedule_MLB() {
+    schedule = null;
+
+    let currDate = new Date(),
+        yyyy = currDate.getFullYear(),
+        mm = currDate.getMonth();
+
+    let firstDay = new Date(yyyy, mm, 1).toISOString();
+    firstDay = firstDay.substring(0, firstDay.indexOf("T"));
+
+    let lastDay = new Date(yyyy, mm + 1, 0).toISOString();
+    lastDay = lastDay.substring(0, lastDay.indexOf("T"));
+
+    $.ajax({
+        url: "https://statsapi.mlb.com/api/v1/schedule?sportId=1&startDate=" + firstDay + "&endDate=" + lastDay,
+        accepts: {
+            text: "application/json"
+        },
+        success: function(games) {
+            if (!games) {
+                console.log("[RetrieveSchedule_MLB]: Retrieved schedule object is null");
+                return;
+            }
+            let scheduleArr = games.dates.filter(date => (date.games.filter(game => (game.teams.away.team.id == selectedTeam.id) || (game.teams.home.team.id == selectedTeam.id))));
+            console.log(scheduleArr)
+            BuildSchedule_MLB(scheduleArr);
+        }
+    })
+}
+
+function RetrieveSchedule_NHL() {
     schedule = null;
 
     let currDate = new Date(),
@@ -176,17 +258,65 @@ function RetrieveSchedule() {
         },
         success: function(games) {
             if (!games) {
-                console.log("[RetrieveSchedule]: Retrieved schedule object is null");
+                console.log("[RetrieveSchedule_NHL]: Retrieved schedule object is null");
                 return;
             }
             let scheduleJson = games.dates;
 
-            BuildSchedule(scheduleJson);
+            BuildSchedule_NHL(scheduleJson);
         }
     })
 }
 
-function BuildSchedule(scheduleJson = null) {
+function BuildSchedule_MLB(scheduleArr = null) {
+    $("#wallpaper-viewer").addClass("spinner");
+
+    let timeZone_Val = $("#TimeZone").val();
+    if (scheduleArr) {
+        schedule = [];
+
+        scheduleArr.forEach(gameData => {
+            let currScheduleObj = {};
+            let currGame = gameData.games[0];
+
+            currScheduleObj.date = TZIntl.getDateTime(timeZone_Val, currGame.gameDate);
+
+            let timeHour = (currScheduleObj.date.hour > 12 ? currScheduleObj.date.hour - 12 : (currScheduleObj.date.hour == 0 ? 12 : currScheduleObj.date.hour));
+            let timeMin = (currScheduleObj.date.minute < 10 ? "0" + currScheduleObj.date.minute : currScheduleObj.date.minute);
+            currScheduleObj.date.dateText = timeHour + ":" + timeMin;
+
+            currScheduleObj.home = (currGame.teams.home.team.id == selectedTeam.id);
+
+            let opponent = (currScheduleObj.home ?
+                currGame.teams.away.team :
+                currGame.teams.home.team);
+
+            let opponentObj = selectedLeague.teams.find(team => team.id == opponent.id);
+
+            currScheduleObj.opponent = {
+                id: opponentObj.id,
+                abbreviation: opponentObj.abbreviation,
+                logo: "../../leagues/nhl/logos/" + opponentObj.id + "/Primary.png"
+            }
+
+            schedule.push(currScheduleObj);
+        })
+    } else if (schedule) {
+        schedule.forEach(gameData => {
+            gameData.date = TZIntl.getDateTime(timeZone_Val, gameData.date.toISOString());
+
+            let timeHour = (gameData.date.hour > 12 ? gameData.date.hour - 12 : (gameData.date.hour == 0 ? 12 : gameData.date.hour));
+            let timeMin = (gameData.date.minute < 10 ? "0" + gameData.date.minute : gameData.date.minute);
+            gameData.date.dateText = timeHour + ":" + timeMin;
+        })
+    }
+
+    scheduleRetrieved = true;
+
+    CreateWallpaper();
+}
+
+function BuildSchedule_NHL(scheduleJson = null) {
     $("#wallpaper-viewer").addClass("spinner");
 
     let timeZone_Val = $("#TimeZone").val();
@@ -209,7 +339,7 @@ function BuildSchedule(scheduleJson = null) {
                 currGame.teams.away.team :
                 currGame.teams.home.team);
 
-            let opponentObj = nhlJson.teams.find(team => team.id == opponent.id);
+            let opponentObj = selectedLeague.teams.find(team => team.id == opponent.id);
 
             currScheduleObj.opponent = {
                 id: opponentObj.id,
@@ -247,4 +377,10 @@ function DownloadWallpaper() {
     let filename = selectedTeam.abbreviation + "_" + month;
 
     saveAs(imageUrl, filename);
+}
+
+
+const findLeagueById = (id) => {
+    const league = Object.keys(leagues).find(league => leagues[league].id == id);
+    return leagues[league];
 }
